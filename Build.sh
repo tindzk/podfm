ba44d086752508eaea7886fc77affb58ae106610
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-12-01 14:55:53 +0100

    Restructure code.

---
+++ Build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mkdir -p Build/{podfm,Jivai}
+
+../Jivai-Utils/jutils.bin        \
+	build                        \
+	output=podfm.bin             \
+	main=Source/Main.c           \
+	manifest=Manifest.h          \
+	include=../Jivai/src         \
+	map=Source:Build/podfm       \
+	map=../Jivai/src:Build/Jivai \
+	optimlevel=0                 \
+	inclhdr=config.h             \
+	dbgsym=yes                   \
+	add=Source/Providers/*.c     \
+	link=@bfd                    \
+	link=@m
--- config.h
+++ config.h
@@ -1,14 +1,3 @@
+#import "../Jivai/config.h"
+
 #define DefaultPath "."
-
-#define Memory_BoundaryChecks 1
-#define Memory_PointerChecks  1
-
-#define Exception_SaveOrigin 1
-#define Exception_SaveTrace  1
-#define Exception_TraceSize 20
-
-#define Logger_DisabledLevels 0
-
-#define Backtrace_HasBFD
-
-#import "Manifest.h"
